844fd0b5d80e18a32fe56959abdb016cae9f7c73
edwinbermudez20 <edwin.bermudez20@gmail.com>
2021-03-19 22:16:26 -0500

    Se versionan fuentes iniciales para crear la rama master

---
+++ src/main/java/co/com/grupoasd/activosfijos/ActivosfijosApplication.java
@@ -0,0 +1,13 @@
+package co.com.grupoasd.activosfijos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivosfijosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivosfijosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/grupoasd/activosfijos/ActivosfijosApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.grupoasd.activosfijos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivosfijosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
